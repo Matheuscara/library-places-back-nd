ddf3eac66f3a33efb1cb5ad9a8494a3744290872
Matheus Cara <61706310+Matheuscara@users.noreply.github.com>
2022-05-26 22:40:36 -0300

    Feature/Push Heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 const app = express()
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001
 
 // constroller 
 const placesController = require('./src/controllers/places')
